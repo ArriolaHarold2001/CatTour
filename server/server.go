b22cbb5d032fca17c3d65b8371a699bdc6af98c1
Harold Arriola Nucamendi <hnucamendi@y094kw2d0x.attlocal.net>
2022-05-22 17:11:58 -0400

    added go.mod and go.sum to be able to get 3rd party dependencies working on server

--- server/server.go
+++ server/server.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-  "net/http"
+	"net/http"
 
-  "github.com/gorilla/handlers"
-  "github.com/gorilla/mux"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
-func main(){
+func main() {
 
-  router := mux.NewRouter()
-  router.HandleFunc("/", getCatImg).Methods("GET", "OPTIONS")
-  http.ListenAndServe(":8000", handlers.CORS()(router))
+	router := mux.NewRouter()
+	router.HandleFunc("/", getCatImg).Methods("GET", "OPTIONS")
+	http.ListenAndServe(":8000", handlers.CORS()(router))
 }
-
